8f3504f512b83d49a0637074d81c8a7a5faa5536
ekburch <ethankburch@gmail.com>
2022-11-11 14:23:16 -0500

    Starting to restrain panel components

---
+++ FRAR/Assets/_Main/__Scripts/StayWithinBounds.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FRAR
+{
+    public class StayWithinBounds : MonoBehaviour
+    {
+        [SerializeField] Transform m_frontBounds = null;
+        public Transform FrontBounds
+        {
+            get => m_frontBounds;
+            set => m_frontBounds = value;
+        }
+
+        [SerializeField] Transform m_backBounds = null;
+        public Transform BackBounds
+        {
+            get => m_backBounds;
+            set => m_backBounds = value;
+        }
+
+        private Vector3 startPosition;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            startPosition = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (transform.position.y < m_frontBounds.position.y || transform.position.y > m_backBounds.position.y)
+                transform.position = startPosition;
+        }
+    }
+}
